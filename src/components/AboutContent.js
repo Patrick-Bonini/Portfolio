9eea2e010ba7c780a38beae8852c0cff77a74c04
Patrick Bonini <patrick.bonini13@gmail.com>
2023-09-15 12:22:21 -0400

    small change to about

--- src/components/AboutContent.js
+++ src/components/AboutContent.js
@@ -8,7 +8,7 @@ const AboutContent = () => {
     <div className="about">
         <div className="left">
             <h1>Who Am I?</h1>
-            <p>Motivated second year software engineering student with a focus in AI/ML @ uOttawa seeking winter 2024 internship opportunities | Python, Java, C++, JavaScript, ReactJS, Flask</p>
+            <p>Motivated second year software engineering student with a focus in AI/ML @ uOttawa seeking winter 2024 internship opportunities | Python, Java, JavaScript, React, Flask</p>
             <NavLink to="https://drive.google.com/file/d/1w6kfz3CdyJ5cEWoZku8l13PyYbHwVoIZ/view?usp=sharing" target="blank"><button className="btn">Resume</button></NavLink>
         </div>
         <div className="right">
